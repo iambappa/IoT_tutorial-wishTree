de2a76b19962f6c14f2af1704f66c221e9fe76e1
iambappa <30632048+iambappa@users.noreply.github.com>
2020-07-21 00:54:00 +0530

    update to whole code
    update

--- mqtt-to-WTportal.ino
+++ mqtt-to-WTportal.ino
@@ -1,35 +1,20 @@
 /*
  Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds //thingtopic
-  - subscribes to the topic "inTopic", printing out any messages  
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
+* currently no callback implemented
+* you can change the device name inside MQTT msg.
+* mqtt_server = "52.3.175.124";
+* port = 1883;
+* server credentials: "arun", "arun@home"
 */
 
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
 // Update these with values suitable for your network.
 
-const char* ssid = "xxxxxxx";       // use your WiFi router SSID
-const char* password = "xxxxxxx";   // use your WiFi router password
+const char* ssid = "TP-LINK";       // use your WiFi router SSID
+const char* password = "bappaditya";   // use your WiFi router password
 const char* mqtt_server = "52.3.175.124";
 
 WiFiClient espClient;
@@ -70,15 +55,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
+  //---------------- this part not implemented yet ---------------------------
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
+    // but actually the LED is on; this is because it is active low on the ESP-01)
   } else {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
-
+  //------------------------------------------------------------------------------
 }
 
 void reconnect() {
@@ -88,20 +73,9 @@ void reconnect() {
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    /*/ Attempt to connect
-    if (client.connect(clientId.c_str()), "arun", "arun@home") {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      //client.publish("outTopic", "hello world");   // commented out by me as this message will create trouble in db, change to thingtopic if want to use
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    }*/
 
-      if (client.connect(clientId.c_str(), "arun", "arun@home")) {
+    if (client.connect(clientId.c_str(), "arun", "arun@home")) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      //client.publish("outTopic", "{\"thingname\":\"firstthing\", \"temperature\": \"36\"}");
-      // ... and resubscribe
       client.subscribe("inTopic");
     }
       
@@ -134,8 +108,8 @@ void loop() {
   if (now - lastMsg > 15000) {
     lastMsg = now;
     value = random(100);   //++value;
-    snprintf (msg, 50, "{\"thingname\":\"firstthing\", \"a\": \"%d\"}", value);    //(msg, 50, "hello world #%ld", value);
-    //sprintf(G_dht_string, ",\"temperature\":%s,\"humidity\":%s", temp, hum) ;   // -> sample
+    snprintf (msg, 50, "{\"thingname\":\"mqttthing\", \"a\": \"%d\"}", value);    //(msg, 50, "hello world #%ld", value);
+    //sprintf(G_dht_string, ",\"temperature\":%s,\"humidity\":%s", temp, hum) ;   // -> sample from other code
     //msg = "{\"thingname\":\"firstthing\", \"a\": \"36\"}"
     Serial.print("Publish message: ");
     Serial.println(msg);
