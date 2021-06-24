1c5ccfe7c701931541b583ad1c992a08021b5ac6
iambappa <30632048+iambappa@users.noreply.github.com>
2021-06-24 07:39:52 +0530

    Unique device tag for MQTT
    Unique devce tag for MQTT

--- mqtt-to-WTportal.ino
+++ mqtt-to-WTportal.ino
@@ -108,7 +108,7 @@ void loop() {
   if (now - lastMsg > 15000) {
     lastMsg = now;
     value = random(100);   //++value;
-    snprintf (msg, 50, "{\"thingname\":\"xxxxxxxx\", \"a\": \"%d\"}", value);    // get a device unique device tag from us
+    snprintf (msg, 50, "{\"thingname\":\"xxxxxxxx\", \"a\": \"%d\"}", value);    // get a unique device tag from us
     //sprintf(G_dht_string, ",\"temperature\":%s,\"humidity\":%s", temp, hum) ;   // -> sample from other code
     //msg = "{\"thingname\":\"firstthing\", \"a\": \"36\"}"
     Serial.print("Publish message: ");
